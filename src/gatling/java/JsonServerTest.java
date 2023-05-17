160dfc92816a542fd07d29623b922c21517e5a3a
Maksym Bova <maksym.bova@ericsson.com>
2023-05-17 13:00:23 +0200

    gradle init

---
+++ src/gatling/java/JsonServerTest.java
@@ -0,0 +1,48 @@
+import io.gatling.javaapi.core.ChainBuilder;
+import io.gatling.javaapi.core.ScenarioBuilder;
+import io.gatling.javaapi.core.Simulation;
+import io.gatling.javaapi.http.HttpProtocolBuilder;
+import io.gatling.javaapi.http.HttpRequestActionBuilder;
+
+import static io.gatling.javaapi.core.CoreDsl.*;
+import static io.gatling.javaapi.http.HttpDsl.http;
+import static io.gatling.javaapi.http.HttpDsl.status;
+import static java.lang.String.format;
+
+public class JsonServerTest extends Simulation {
+    private HttpRequestActionBuilder postJson() {
+        return http("Post json file")
+                .post("/json")
+                .body(RawFileBody("bodies/sample1.json"))
+                .header("Content-Type", "application/json")
+                .check(status().is(201));
+    }
+
+    private HttpRequestActionBuilder getJson() {
+        return http("Get json file")
+                .get("/json")
+                .header("Content-Type", "application/json")
+                .check(status().is(200));
+    }
+
+    ChainBuilder postAndGetJson =
+            ChainBuilder.EMPTY
+                    .exec(postJson())
+                    .exec(getJson());
+
+    ScenarioBuilder postAndGetScenario = scenario("Execute post and get scenario")
+            .exec(postAndGetJson);
+
+    HttpProtocolBuilder httpProtocol = http.baseUrl(format("%s:%s", "http://localhost", "8082"));
+
+    {
+        setUp(
+                postAndGetScenario.injectOpen(
+                        atOnceUsers(1)
+                )
+
+        ).protocols(httpProtocol);
+
+    }
+
+}
